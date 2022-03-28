bd4815eebbbefb7ede5520cf5c50a9309ddb1f5d
FajarRAP <fajary781@gmail.com>
2022-03-28 10:18:30 +0700

    program perbandingan 2 bilangan

--- P3/Program/3.1.cpp
+++ P3/Program/3.1.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int a, b;
+    cout<<"Masukkan A : ";
+    cin>>a;
+    cout<<"Masukkan B : ";
+    cin>>b;
+    if(a>b){
+        cout<<"Bilangan terbesar adalah A = "<<a<<endl;
+    }else{
+        cout<<"Bilangan terbesar adalah B = "<<b<<endl;
+    }
+    return 0;
 }
