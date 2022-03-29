2407d866cda58c66637ce4a648a80734c3861d7e
FajarRAP <fajary781@gmail.com>
2022-03-29 16:02:06 +0700

    bikin class

--- Program4OOP.cpp
+++ Program4OOP.cpp
@@ -1,6 +1,32 @@
 #include<iostream>
 using namespace std;
 
+class nilai{
+    public:
+        int angka;
+        char huruf;
+
+        int inputangka(){
+            cout<<"Masukkan angka : ";
+            cin>>angka;
+            return angka;
+        }
+
+        void tampilhuruf(){
+            if(angka>0 && angka<=20){
+                huruf='E';
+            }else if(angka>20 && angka<=40){
+                huruf='D';
+            }else if(angka>40 && angka<=60){
+                huruf='C';
+            }else if(angka>60 && angka<=80){
+                huruf='B';
+            }else{
+                huruf='A';
+            }
+            cout<<"Nilai huruf = "<<huruf<<endl;
+        }
+};
 
 int main(){
     
