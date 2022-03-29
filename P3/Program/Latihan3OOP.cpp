f997dacf11093b1cdc51d9cfcd32714fe82bf553
FajarRAP <fajary781@gmail.com>
2022-03-29 21:38:05 +0700

    kelarin fungsi di class

--- P3/Program/Latihan3OOP.cpp
+++ P3/Program/Latihan3OOP.cpp
@@ -3,12 +3,26 @@ using namespace std;
 
 class cek{
     public:
-        int angka;
-        char kecil, kapital;
+        char karakter;
 
-        
+        int inputkarakter(){
+            cout<<"Silakan input 1 karakter : ";
+            cin>>karakter;
+            return karakter;
+        }
+
+        void ngecek(){
+            if(karakter>'1' && karakter<'9'){
+                cout<<"Angka"<<endl;
+            }else if(karakter>'a' && karakter<'z'){
+                cout<<"Huruf Kecil"<<endl;
+            }else if(karakter>'A' && karakter<'Z'){
+                cout<<"Huruf Kapital"<<endl;
+            }
+        }
 };
 
 int main(){
+    
     return 0;
 }
