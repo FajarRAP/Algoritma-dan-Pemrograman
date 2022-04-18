a857384150bd745f8ed6af4c1ea4ac095c133320
FajarRAP <fajary781@gmail.com>
2022-04-19 04:48:43 +0700

    fix fungsi

--- P6/Program, Flowchart/Program3.cpp
+++ P6/Program, Flowchart/Program3.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int logaritma(int n, int x, int y){
     float hasil;
     if(n==0){
-        return n;
-    }else{
         hasil=exp(y*log(x));
         cout<<hasil<<endl;
-        return (n-1, x, y);
+        return n;
+    }else{
+        return logaritma(n-1, x, y);
     }
 }
 
